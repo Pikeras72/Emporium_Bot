f39c0bb5dc24a3b87821e454337d481734cfe635
Diego Ruiz <diego.rpiqueras@alumnos.upm.es>
2022-05-12 09:32:13 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+// TOKEN: OTczNjUyNjE3NDI5Mzk3NTA0.GfDYgI.mCfvZC1JgkbbXVO4uwKJT-_pdLMCikfznZZQ9M
+// INVITE LINK: https://discord.com/api/oauth2/authorize?client_id=973652617429397504&permissions=8&scope=bot
+
+const Discord = require("discord.js");
